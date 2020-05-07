c9416f37d6a0145d15e50e5be2c3eb4fec1036f1
huangdaming <861634426@qq.com>
2020-05-07 10:50:20 +0800

    优雅停机

--- leyou-goods-web/src/main/java/com/leyou/LyGoodsWebApplication.java
+++ leyou-goods-web/src/main/java/com/leyou/LyGoodsWebApplication.java
@@ -1,9 +1,14 @@
 package com.leyou;
 
+import com.config.GracefulShutdownTomcat;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 
 /**
  * @Author: 98050
@@ -16,5 +21,15 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 public class LyGoodsWebApplication {
     public static void main(String[] args) {
         SpringApplication.run(LyGoodsWebApplication.class,args);
+    }
+
+    @Autowired
+    private GracefulShutdownTomcat gracefulShutdownTomcat;
+
+    @Bean
+    public ServletWebServerFactory servletContainer() {
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+        tomcat.addConnectorCustomizers(gracefulShutdownTomcat);
+        return tomcat;
     }
 }
